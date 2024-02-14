a5f55478bad7c5af8f60eec3859e4b45c4accafb
DanielSparta <111179755+DanielSparta@users.noreply.github.com>
2024-02-14 11:54:12 +0200

    save

--- TcpProxy/TcpProxy/Memory/StringAddress.cs
+++ TcpProxy/TcpProxy/Memory/StringAddress.cs
@@ -7,26 +7,23 @@ using System.Threading;
 
 public class StringAddress
 {
-    private delegate void StringReplacementEventHandler(object sender, StringReplacementEventArgs e);
-    private static event StringReplacementEventHandler StringReplaced;
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesRead);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+    [DllImport("kernel32.dll", SetLastError = true)]
+    public static extern bool CloseHandle(IntPtr hObject);
 
     [DllImport("kernel32.dll")]
-    private static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesRead);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
-
-    [DllImport("kernel32.dll", SetLastError = true)]
-    private static extern bool CloseHandle(IntPtr hObject);
-
-    [DllImport("kernel32.dll")]
-    private static extern IntPtr VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
+    public static extern IntPtr VirtualQueryEx(IntPtr hProcess, IntPtr lpAddress, out MEMORY_BASIC_INFORMATION lpBuffer, uint dwLength);
 
     [StructLayout(LayoutKind.Sequential)]
-    private struct MEMORY_BASIC_INFORMATION
+    public struct MEMORY_BASIC_INFORMATION
     {
         public IntPtr BaseAddress;
         public IntPtr AllocationBase;
@@ -76,15 +73,8 @@ public class StringAddress
                         int index = FindBytes(buffer, Encoding.ASCII.GetBytes(searchString));
                         while (index != -1)
                         {
-                            IntPtr stringAddress = IntPtr.Add(memoryInfo.BaseAddress, index);
-                            Console.WriteLine($"Found string at address: 0x{stringAddress.ToString("X")}");
-
                             byte[] replaceBuffer = Encoding.ASCII.GetBytes(replaceString.PadRight(searchString.Length, '\0'));
                             WriteProcessMemory(processHandle, IntPtr.Add(memoryInfo.BaseAddress, index), replaceBuffer, (uint)replaceBuffer.Length, out int bytesWritten);
-
-                            // Print a message indicating the replacement
-                            Console.WriteLine($"New address found, and now is changing into {replaceString}");
-
                             index = FindBytes(buffer, Encoding.ASCII.GetBytes(searchString), index + replaceBuffer.Length);
                         }
                     }
@@ -99,7 +89,7 @@ public class StringAddress
         }
     }
 
-    static int FindBytes(byte[] haystack, byte[] needle, int startIndex = 0)
+    private static int FindBytes(byte[] haystack, byte[] needle, int startIndex = 0)
     {
         for (int i = startIndex; i <= haystack.Length - needle.Length; i++)
         {
@@ -118,17 +108,5 @@ public class StringAddress
             }
         }
         return -1;
-    }
-}
-
-class StringReplacementEventArgs : EventArgs
-{
-    public string OriginalString { get; }
-    public string ReplacedString { get; }
-
-    public StringReplacementEventArgs(string originalString, string replacedString)
-    {
-        OriginalString = originalString;
-        ReplacedString = replacedString;
     }
 }
